6847208b3875f79a41d9a61b9325bf44da3ae0b6
borisvorob <borisss777@gmail.com>
2018-09-11 14:47:22 +0300

    Update

---
+++ docs/precache-manifest.04f9fd5a86428117289808a0adf1a359.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "fonts/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "ffc47e74e8f7aa84ad937e525c5e1e4d",
+    "url": "fonts/crcbold.ffc47e74.woff"
+  },
+  {
+    "revision": "8b64eeaa425ddd79697c",
+    "url": "js/app.91be112e.js"
+  },
+  {
+    "revision": "47df209c6397c2c4d29f",
+    "url": "js/chunk-vendors.76c81b3c.js"
+  },
+  {
+    "revision": "bbc0770ffce8efae025d",
+    "url": "js/about.092000ac.js"
+  },
+  {
+    "revision": "90bea94880b0286ec5c9af21f3d6fc9c",
+    "url": "index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "8d564363d664c82df13fdd5ba04316f6",
+    "url": "img/header.8d564363.png"
+  },
+  {
+    "revision": "2624785eb5110404c1b1c815f9c86202",
+    "url": "fonts/crc55.2624785e.woff"
+  },
+  {
+    "revision": "50037ffe1d78ae8fbc86f772c4fb0774",
+    "url": "fonts/crc35.50037ffe.woff"
+  },
+  {
+    "revision": "0c2242c8a9bc96a0f6feaa56911e4434",
+    "url": "fonts/Circe-ExtraBold.0c2242c8.woff"
+  },
+  {
+    "revision": "8b64eeaa425ddd79697c",
+    "url": "css/app.7634f06e.css"
+  }
+];
