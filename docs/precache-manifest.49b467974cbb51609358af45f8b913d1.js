735505c3c67812255e25e6b79fae0665b1e4c48b
Boris Perepelita <boris.perepelita@intelligentbee.com>
2018-11-08 11:09:14 +0200

    Update

---
+++ docs/precache-manifest.49b467974cbb51609358af45f8b913d1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ba66407adbeb4a540c57f010f193be0",
+    "url": "fonts/Gilmer-Medium.6ba66407.woff2"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "fonts/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "dad477a43c94b843ac91",
+    "url": "js/app.d9ab8de3.js"
+  },
+  {
+    "revision": "c23892d92743d0bb87d4",
+    "url": "js/chunk-vendors.2c5e12bb.js"
+  },
+  {
+    "revision": "bbc0770ffce8efae025d",
+    "url": "js/about.092000ac.js"
+  },
+  {
+    "revision": "d0f642f5b5ab390e4b4fa378a12df8bb",
+    "url": "index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "a96f0b36b60db5d2fc6b734e1bf8b357",
+    "url": "img/header.a96f0b36.jpg"
+  },
+  {
+    "revision": "1d2c82f9caff304fe2b5dd1d72209630",
+    "url": "fonts/Gilmer-Regular.1d2c82f9.woff2"
+  },
+  {
+    "revision": "5c7d040943470ac1ff06937e1e717889",
+    "url": "fonts/Gilmer-Light.5c7d0409.woff2"
+  },
+  {
+    "revision": "8d5a96bef907eb8f9f69613b6df6a3b8",
+    "url": "fonts/Gilmer-Bold.8d5a96be.woff2"
+  },
+  {
+    "revision": "bfb0a117633576d02bfd7a0a47a37fbb",
+    "url": "fav.png"
+  },
+  {
+    "revision": "dad477a43c94b843ac91",
+    "url": "css/app.75fbe206.css"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.d8671b6f516b65e62f6fd1b1fed0b639.js"
+  "precache-manifest.49b467974cbb51609358af45f8b913d1.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "sbc"});
